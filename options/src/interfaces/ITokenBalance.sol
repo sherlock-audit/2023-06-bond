8953a3531a514d4f5a0c3a4f1ad28c306095b7ee
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-06-29 12:58:19 +0200

    Delete options directory

--- options/src/interfaces/ITokenBalance.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity >=0.8.0;
-
-/// @notice Generic interface for tokens that implement a balanceOf function (includes ERC-20 and ERC-721)
-interface ITokenBalance {
-    /// @notice Get the user's token balance
-    function balanceOf(address user_) external view returns (uint256);
-}
